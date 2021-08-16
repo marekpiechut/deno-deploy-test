79e26a0760af6c31e3921ac455452fbb6bd22477
Marek Piechut <marek@dayone.pl>
2021-08-16 14:21:02 +0200

    Just a first commit

---
+++ mod.ts
@@ -0,0 +1,41 @@
+import { Application, Router } from 'https://deno.land/x/oak/mod.ts'
+import {
+  map,
+  reduce,
+} from 'https://deno.land/x/lodash@4.17.19/dist/lodash.min.js'
+
+interface IBook {
+  id: string
+  title: string
+  author: string
+}
+const books = new Map<string, IBook>()
+books.set('1', {
+  id: '1',
+  title: 'The Hound of the Baskervilles',
+  author: 'Conan Doyle, Arthur',
+})
+
+const router = new Router()
+router
+  .get('/', context => {
+    const titles = map(books, 'title')
+    context.response.body = { titles: reduce(titles, (a, t) => `${a}, ${t}`) }
+  })
+  .get('/books', context => {
+    context.response.body = Array.from(books.values())
+  })
+  .get('/book/:id', context => {
+    if (context.params && context.params.id && books.has(context.params.id)) {
+      context.response.body = books.get(context.params.id)
+    }
+  })
+  .get('/port', ctx => {
+    ctx.response.body = { PORT: Deno.env.get('PORT') }
+  })
+
+const app = new Application()
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+addEventListener('fetch', app.fetchEventHandler())
